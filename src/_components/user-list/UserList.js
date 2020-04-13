94d7331c1963b68f56f0f0ad02877221c2500d13
90adityaCode <90adityacode@gmail.com>
2020-04-13 17:03:25 +0530

    1 file modified

--- src/_components/user-list/UserList.js
+++ src/_components/user-list/UserList.js
@@ -6,32 +6,7 @@ class UserList extends Component {
     super(props);
     this.state = {
       trainer: "",
-      userList: [
-        {
-          id: 1,
-          email: "michael.lawson@reqres.in",
-          first_name: "Michael",
-          last_name: "Lawson",
-          avatar:
-            "https://s3.amazonaws.com/uifaces/faces/twitter/follettkyle/128.jpg"
-        },
-        {
-          id: 2,
-          email: "lindsay.ferguson@reqres.in",
-          first_name: "Lindsay",
-          last_name: "Ferguson",
-          avatar:
-            "https://s3.amazonaws.com/uifaces/faces/twitter/araa3185/128.jpg"
-        },
-        {
-          id: 3,
-          email: "tobias.funke@reqres.in",
-          first_name: "Tobias",
-          last_name: "Funke",
-          avatar:
-            "https://s3.amazonaws.com/uifaces/faces/twitter/vivekprvr/128.jpg"
-        }
-      ],
+      userList: [],
       userDetail: {}
     };
 
@@ -103,7 +78,7 @@ class UserList extends Component {
   componentDidMount() {
     const _userService = new UserService();
     _userService.fetchUser().then(userList => {
-      console.log("userList",userList );
+      this.setState({userList: userList.data})
     });
   }
 }
