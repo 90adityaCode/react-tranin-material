1d632a47551aef0177cfca9723b527dfbe09dc76
90adityaCode <90adityacode@gmail.com>
2020-04-16 21:52:43 +0530

    1 file modified, 1 added

---
+++ src/_router/Routers.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import UserList from './_components/user-list/UserList';
+import Blog from './_components/blog/Blog';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+
+const Routers = ()=>{
+  return (
+    <Router>
+    <Switch>
+      <Route path="/users" component={UserList}/>
+      <Route path="/blog" component={Blog}/> 
+      <Redirect to='/' />
+    </Switch>
+       
+    </Router>
+  )
+}
+export default Routers;
